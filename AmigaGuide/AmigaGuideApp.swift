aab525be5e5f567796f5ffe52a64aa6284dc39e7
Matt Sephton <matt.sephton@gmail.com>
2026-08-30 18:18:48 +0100

    Initial Commit

---
+++ AmigaGuide/AmigaGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmigaGuideApp.swift
+//  AmigaGuide
+//
+//  Created by Matt Sephton on 2026-08-30.
+//
+
+import SwiftUI
+
+@main
+struct AmigaGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmigaGuideTests/AmigaGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  AmigaGuideTests.swift
+//  AmigaGuideTests
+//
+//  Created by Matt Sephton on 2026-08-30.
+//
+
+import Testing
+@testable import AmigaGuide
+
+struct AmigaGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
